1c954bcca00f2f9c21a0cff34cfaa0d509558ebd
Tiago Funk <tiagoff.tf@gmail.com>
2020-05-14 23:03:22 -0300

    experiment done

--- script.py
+++ script.py
@@ -10,9 +10,16 @@ instances = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))
 
 arguments=[program, "1", "2", "3", "", "--strategy", "mpr", "--sizePopulation", "10", "--elite", "0.40", "--limitIterations", "10", "--sizePlasmideo", "0.50", "--cross", "0.85", "--selectionStrategy", "a", "--intermediaryStrategy", "stef"]
 
-for i in range( 4 ):
+arquivo = open('log.txt', 'w')
+
+for i in range( 30 ):
     for j in instances:
         arguments[4] = j
         py2output = subprocess.check_output( arguments )
-        print(j, py2output)
+        arquivo.write( j.replace("Instances/","").replace(".pcar","") )
+        arquivo.write( "," )
+        arquivo.write( str( py2output ).replace("b","").replace("\'","") )
+        arquivo.write( "\n" )
     print( ">>>>>>>>> ", i )
+    
+arquivo.close()
